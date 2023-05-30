4fe762f4ff0d82af20409641fec87d0ae696234c
Ahmed Mehana <102672897+ahmed7mehana@users.noreply.github.com>
2023-05-30 13:23:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Footer from './Components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
